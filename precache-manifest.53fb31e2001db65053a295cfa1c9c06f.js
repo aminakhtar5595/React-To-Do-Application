80205b3ce476f2f5aac6e96ebae59ed96d4f2ba0
Amin Akhtar <aminakhtar5595@gmail.com>
2019-11-30 10:33:28 +0500

    Updates

---
+++ precache-manifest.53fb31e2001db65053a295cfa1c9c06f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "198721d8c01bdb031e36",
+    "url": "/React-To-Do-Application/static/js/runtime~main.907fe2cd.js"
+  },
+  {
+    "revision": "d0dff013fb1d31015b78",
+    "url": "/React-To-Do-Application/static/js/main.43fa70b4.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React-To-Do-Application/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d0dff013fb1d31015b78",
+    "url": "/React-To-Do-Application/static/css/main.fa85694e.chunk.css"
+  },
+  {
+    "revision": "8834433203007cf94538ce7ddbd99c5b",
+    "url": "/React-To-Do-Application/index.html"
+  }
+];
